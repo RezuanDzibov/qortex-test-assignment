d67a24aae80b9b4b1c9536389e4ea46c5419f62c
rezuan <rezuan.dzbov@gmail.com>
2023-02-18 17:12:56 +0300

    register models in admin panel

--- songs/admin.py
+++ songs/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    pass
+
+
+class AlbumSongInline(admin.StackedInline):
+    model = models.AlbumSong
+
+
+@admin.register(models.Album)
+class AlbumAdmin(admin.ModelAdmin):
+    inlines = [AlbumSongInline]
+
+
+@admin.register(models.Song)
+class SongAdmin(admin.ModelAdmin):
+    pass
+
