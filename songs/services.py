f8cea9b338af000508383d847f3e1252f0ec5f4b
rezuan <rezuan.dzbov@gmail.com>
2023-02-18 22:02:22 +0300

    add get_album, get_song, add_song_to_album, create_song services

---
+++ songs/services.py
@@ -0,0 +1,45 @@
+from django.db import IntegrityError, transaction
+from django.shortcuts import get_object_or_404
+
+from . import models, serializers
+
+
+def get_album(id_: int) -> models.Album:
+    album = get_object_or_404(models.Album, pk=id_)
+    return album
+
+
+def get_song(id_: int) -> models.Song:
+    song = get_object_or_404(models.Song, pk=id_)
+    return song
+
+
+def add_song_to_album(data: dict) -> models.Album:
+    serializer = serializers.AddSongToAlbumSerializer(data=data)
+    if serializer.is_valid(raise_exception=True):
+        album = get_album(id_=serializer.validated_data["album"])
+        song = get_song(id_=serializer.validated_data["song"])
+        models.AlbumSong.objects.create(
+            order_number=serializer.validated_data["order_number"],
+            song=song,
+            album=album
+        )
+        album.refresh_from_db()
+        return album
+
+
+def create_song(data: dict) -> models.AlbumSong:
+    serializer = serializers.CreateSongSerializer(data=data)
+    if serializer.is_valid(raise_exception=True):
+        album = get_album(id_=serializer.validated_data.get("album"))
+        try:
+            with transaction.atomic():
+                song = models.Song.objects.create(title=serializer.validated_data["title"])
+                album_song = models.AlbumSong.objects.create(
+                    order_number=serializer.validated_data["order_number"],
+                    song=song,
+                    album=album
+                )
+            return album_song
+        except IntegrityError as exc:
+            pass
