b27cd083761dd9a1992366ef777f52c29f9eb78a
rezuan <rezuan.dzbov@gmail.com>
2023-02-18 22:00:39 +0300

    add ArtistViewSet, AlbumViewSet, AddSongToAlbumView, CreateSongView

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
 
-# Create your views here.
+from . import models, serializers, services
+
+
+class ArtistViewSet(ModelViewSet):
+    queryset = models.Artist.objects.all()
+    serializer_class = serializers.ArtistSerializer
+    http_method_names = ["get", "post", "put", "delete"]
+
+
+class AlbumViewSet(ModelViewSet):
+    queryset = models.Album.objects.all()
+    serializer_class = serializers.AlbumSerializer
+    http_method_names = ["get", "post", "put", "delete"]
+
+    @swagger_auto_schema(responses={200: serializers.AlbumRetrieveSerializer()})
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = serializers.AlbumRetrieveSerializer(instance=instance)
+        return Response(serializer.data)
+
+
+class AddSongToAlbumView(APIView):
+    @swagger_auto_schema(
+        request_body=serializers.AddSongToAlbumSerializer,
+        responses={200: serializers.AlbumRetrieveSerializer()}
+    )
+    def post(self, request, *args, **kwargs):
+        album = services.add_song_to_album(data=request.data.copy())
+        serializer = serializers.AlbumRetrieveSerializer(instance=album)
+        return Response(serializer.data)
+
+
+class CreateSongView(APIView):
+    @swagger_auto_schema(
+        request_body=serializers.CreateSongSerializer,
+        responses={200: serializers.SongCreateRetrieve()}
+    )
+    def post(self, request, *args, **kwargs):
+        song = services.create_song(data=request.data.copy())
+        serializer = serializers.SongCreateRetrieve(instance=song)
+        return Response(serializer.data)
