fccea002893aaf2cd598698df2261483e1bd159a
rezuan <rezuan.dzbov@gmail.com>
2023-02-19 11:13:56 +0300

    register RemoveSongFromAlbumView in urls

--- songs/urls.py
+++ songs/urls.py
@@ -9,7 +9,8 @@ router.register("albums", views.AlbumViewSet, basename="albums")
 
 urlpatterns = [
     path("albums/add_song/", views.AddSongToAlbumView.as_view(), name="add_song_to_album"),
-    path("songs/", views.CreateSongView.as_view(), name="create_song")
+    path("albums/remove_song/", views.RemoveSongFromAlbum.as_view(), name="remove_song_from_album"),
+    path("songs/", views.CreateSongView.as_view(), name="create_song"),
 ]
 
 urlpatterns += router.urls
