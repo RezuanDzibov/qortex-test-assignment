eaeb05bed4ca8940671a2a93ee02de161fffac66
rezuan <rezuan.dzbov@gmail.com>
2023-02-19 20:39:24 +0300

    Fix api name in swagger

--- config/yasg.py
+++ config/yasg.py
@@ -5,7 +5,7 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Manager API",
+        title="Songs API",
         default_version="v1",
     ),
     public=True,
