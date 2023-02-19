5e270f7ac5d7e5181edc3c9e4469b356e3288837
rezuan <rezuan.dzbov@gmail.com>
2023-02-19 20:23:34 +0300

    add get_current_year util

--- songs/utils.py
+++ songs/utils.py
@@ -1,5 +1,11 @@
+from datetime import date
+
 from . import models
 
 
 def get_next_order_number(album: models.Album) -> int:
     return album.songs.all().count() + 1
+
+
+def get_current_year() -> int:
+    return date.today().year
