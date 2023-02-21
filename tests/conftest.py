3b4a990da0ca36a75cd7e7ca774792f1e53073aa
rezuan <rezuan.dzbov@gmail.com>
2023-02-21 17:03:55 +0300

    add built_artist fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,15 @@
 import pytest
-
 from rest_framework.test import APIClient
+
+from . import factories
+from songs import models
 
 
 @pytest.fixture(scope="session")
 def api_client() -> APIClient:
     return APIClient()
 
+
+@pytest.fixture(scope="function")
+def built_artist() -> models.Artist:
+    return factories.ArtistFactory.build()
