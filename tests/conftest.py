fa867e08df96c54b88d4fb6847363cd72a2c70dc
rezuan <rezuan.dzbov@gmail.com>
2023-02-22 19:53:19 +0300

    add artist pytest fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,3 +13,9 @@ def api_client() -> APIClient:
 @pytest.fixture(scope="function")
 def built_artist() -> models.Artist:
     return factories.ArtistFactory.build()
+
+
+@pytest.fixture(scope="function")
+def artist(db, built_artist: models.Artist) -> models.Artist:
+    return factories.ArtistFactory.create()
+
