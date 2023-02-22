e10cdeff0f17a07a94f3a07143e839ea017102c2
Rezuan Dzibov <68289805+RezuanDzibov@users.noreply.github.com>
2023-02-22 18:15:07 +0300

    TestCreateArstit (#4)
    * add url class variable
    
    * add test_invalid_data case

--- tests/test_endpoints/test_artist.py
+++ tests/test_endpoints/test_artist.py
@@ -5,6 +5,12 @@ from songs.models import Artist
 
 
 class TestCreateArtist:
+    url = reverse("artists-list")
+
     def test_successful(self, db, api_client: APIClient, built_artist: Artist):
-        response = api_client.post(reverse("artists-list"), data={"name": built_artist.name})
+        response = api_client.post(self.url, data={"name": built_artist.name})
         assert response.status_code == 201
+
+    def test_invalid_data(self, db, api_client: APIClient):
+        response = api_client.post(self.url, data={"name": "a" * 256})
+        assert response.status_code == 400
