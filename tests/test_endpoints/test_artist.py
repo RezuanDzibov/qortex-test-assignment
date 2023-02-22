3b06129b9dd945849639b3be339bfa75857d2028
rezuan <rezuan.dzbov@gmail.com>
2023-02-22 18:07:59 +0300

    add url class variable

--- tests/test_endpoints/test_artist.py
+++ tests/test_endpoints/test_artist.py
@@ -5,6 +5,8 @@ from songs.models import Artist
 
 
 class TestCreateArtist:
+    url = reverse("artists-list")
+
     def test_successful(self, db, api_client: APIClient, built_artist: Artist):
-        response = api_client.post(reverse("artists-list"), data={"name": built_artist.name})
+        response = api_client.post(self.url, data={"name": built_artist.name})
         assert response.status_code == 201
