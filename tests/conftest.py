bd8a477fbd1f3b440b1986a52efb72a315ac217c
Rezuan Dzibov <68289805+RezuanDzibov@users.noreply.github.com>
2023-02-23 15:26:37 +0300

    Test retrieve artist (#5)
    * add artist pytest fixture
    
    * add TestRetrieveArtist
    
    * add artists pytest fixture
    
    * add test_multiple_artists_exist, test_not_found, test_not_exists_artist test cases to TestRetrieveArtist
    
    * remove random elements

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
+from functools import partial
+from random import randint
+from typing import List
+
 import pytest
 from rest_framework.test import APIClient
 
@@ -13,3 +17,18 @@ def api_client() -> APIClient:
 @pytest.fixture(scope="function")
 def built_artist() -> models.Artist:
     return factories.ArtistFactory.build()
+
+
+@pytest.fixture(scope="function")
+def artist(db, built_artist: models.Artist) -> models.Artist:
+    return factories.ArtistFactory.create()
+
+
+@pytest.fixture(scope="function")
+def artists(request, db) -> [models.Artist]:
+    func = partial(factories.ArtistFactory.create_batch)
+    if hasattr(request, "param") and request.param is int and request.param > 0:
+        artists = func(request.param)
+    else:
+        artists = func(randint(1, 6))
+    return artists
--- tests/test_endpoints/test_artist.py
+++ tests/test_endpoints/test_artist.py
@@ -14,3 +14,21 @@ class TestCreateArtist:
     def test_invalid_data(self, db, api_client: APIClient):
         response = api_client.post(self.url, data={"name": "a" * 256})
         assert response.status_code == 400
+
+
+class TestRetrieveArtist:
+    def test_successful(self, api_client: APIClient, artist: Artist):
+        response = api_client.get(reverse("artists-detail", kwargs={"pk": artist.id}))
+        assert response.status_code == 200
+
+    def test_multiple_artists_exist(self, api_client: APIClient, artists: [Artist]):
+        response = api_client.get(reverse("artists-detail", kwargs={"pk": artists[1].id}))
+        assert response.status_code
+
+    def test_not_found(self, db, api_client: APIClient):
+        response = api_client.get(reverse("artists-detail", kwargs={"pk": 1000}))
+        assert response.status_code == 404
+
+    def test_not_exists_artist(self, api_client: APIClient, artists: [Artist]):
+        response = api_client.get(reverse("artists-detail", kwargs={"pk": 1000}))
+        assert response.status_code == 404
