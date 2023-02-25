236f92a7330fb0e0b351a2595f077a5df5cc8e2d
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-02-25 18:33:56 +0300

    add test_not_exists to TestGetAlbum

--- tests/test_services/test_album.py
+++ tests/test_services/test_album.py
@@ -1,3 +1,6 @@
+import pytest
+from django.http import Http404
+
 from songs import services
 from songs.models import Album
 
@@ -11,3 +14,8 @@ class TestGetAlbum:
         album = albums[0]
         album_in_db = services.get_album(id_=album.id)
         assert album == album_in_db
+
+    def test_not_exists(self, albums: [Album]):
+        with pytest.raises(Http404) as exc:
+            services.get_album(id_=1000)
+        assert isinstance(exc.value, Http404)
