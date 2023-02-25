41cad394fb26037dae1376ddadb3dd6c5384a369
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-02-25 18:25:13 +0300

    add test_successful_multiple_exist to TestGetAlbum

--- tests/test_services/test_album.py
+++ tests/test_services/test_album.py
@@ -6,3 +6,8 @@ class TestGetAlbum:
     def test_successful(self, album: Album):
         album_in_db = services.get_album(id_=album.id)
         assert album == album_in_db
+
+    def test_successful_multiple_exist(self, albums: [Album]):
+        album = albums[0]
+        album_in_db = services.get_album(id_=album.id)
+        assert album == album_in_db
