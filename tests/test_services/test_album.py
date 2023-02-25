5a56f2525a46b7c746d5f00a97301a0c19f7a84d
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-02-25 18:23:20 +0300

    add test_successful to TestGetAlbum

--- tests/test_services/test_album.py
+++ tests/test_services/test_album.py
@@ -1,2 +1,8 @@
+from songs import services
+from songs.models import Album
+
+
 class TestGetAlbum:
-    pass
+    def test_successful(self, album: Album):
+        album_in_db = services.get_album(id_=album.id)
+        assert album == album_in_db
