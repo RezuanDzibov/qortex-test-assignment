240a98b4ed04f0001bf7540e06d5fa317b0e98c1
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-03-05 11:02:21 +0300

    add test_successful case to TestCreateAlbum

--- tests/test_endpoints/test_album.py
+++ tests/test_endpoints/test_album.py
@@ -1,2 +1,18 @@
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+from songs.models import Album
+
+
 class TestCreateAlbum:
-    pass
+    url = reverse("albums-list")
+
+    def test_successful(self, api_client: APIClient, built_album: Album):
+        response = api_client.post(
+            self.url,
+            data={
+                "release_year": built_album.release_year,
+                "artist": built_album.artist.id
+            }
+        )
+        assert response.status_code == 201
