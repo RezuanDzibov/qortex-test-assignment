ccab6f8cd8fae5b7f28c5717f9a2101a250a7f5c
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-03-05 11:04:49 +0300

    add test_with_invalid_data case to TestCreateAlbum

--- tests/test_endpoints/test_album.py
+++ tests/test_endpoints/test_album.py
@@ -16,3 +16,14 @@ class TestCreateAlbum:
             }
         )
         assert response.status_code == 201
+
+    def test_with_invalid_data(self, api_client: APIClient, built_album: Album):
+        built_album.release_year = "invalid_year"
+        response = api_client.post(
+            self.url,
+            data={
+                "release_year": built_album.release_year,
+                "artist": built_album.artist.id
+            }
+        )
+        assert response.status_code == 400
