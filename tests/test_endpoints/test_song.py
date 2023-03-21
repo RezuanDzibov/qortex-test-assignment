e2bf0ff1487175f125afbd1d8183402a76bfa513
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-03-21 17:41:21 +0300

    add test_create_one case to TestCreateSong

--- tests/test_endpoints/test_song.py
+++ tests/test_endpoints/test_song.py
@@ -1,2 +1,16 @@
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+from songs.models import Song
+
+
 class TestCreateSong:
-    pass
+    url = reverse("songs-list")
+
+    def test_create_one(self, db, api_client: APIClient, built_song: Song):
+        data = {"title": built_song.title}
+        response = api_client.post(self.url, data)
+        response_data = response.data.copy()
+        response_data.pop("id")
+        assert response.status_code == 201
+        assert response_data == data
