e176be97a42218bdc185ad72584eb48d9217e269
Rezuan Dzibov <68289805+RezuanDzibov@users.noreply.github.com>
2023-03-21 19:59:51 +0300

    add TestSongRetrieve (#26)
    * add TestRetrieveSong
    
    * add test_get_one case to tTestRetrieveSong
    
    * add test_multiple_exist case to tTestRetrieveSong
    
    * add test_none_exist case to tTestRetrieveSong
    
    * add test_not_exists case to tTestRetrieveSong

--- tests/test_endpoints/test_song.py
+++ tests/test_endpoints/test_song.py
@@ -1,7 +1,10 @@
+from functools import partial
+
 from django.urls import reverse
 from rest_framework.test import APIClient
 
 from songs.models import Song
+from songs.serializers import SongRetrieveSerializer
 
 
 class TestCreateSong:
@@ -22,3 +25,25 @@ class TestCreateSong:
     def test_with_not_exists_field(self, db, api_client: APIClient):
         response = api_client.post(self.url, data={"field": "value"})
         assert response.status_code == 400
+
+
+class TestRetrieveSong:
+    url = partial(reverse, "songs-detail")
+
+    def test_get_one(self, api_client: APIClient, song: Song):
+        response = api_client.get(self.url(kwargs={"pk": song.id}))
+        assert response.status_code == 200
+        assert response.data == SongRetrieveSerializer(song).data
+
+    def test_multiple_exist(self, api_client: APIClient, songs: [Song]):
+        response = api_client.get(self.url(kwargs={"pk": songs[1].id}))
+        assert response.status_code
+        assert response.data == SongRetrieveSerializer(songs[1]).data
+
+    def test_none_exist(self, db, api_client: APIClient):
+        response = api_client.get(self.url(kwargs={"pk": 1000}))
+        assert response.status_code == 404
+
+    def test_not_exists(self, api_client: APIClient, songs: [Song]):
+        response = api_client.get(self.url(kwargs={"pk": 1000}))
+        assert response.status_code == 404
